c41f0ce7ceaa4f0111eb9e3efc9c9ca2f24c501b
Muhammad Abdurrohman <mabdurrohman26@dev1.idserve.net>
2026-02-03 10:17:27 +0700

    add promhttp

--- internal/monitoring/prometheus.go
+++ internal/monitoring/prometheus.go
@@ -1,11 +1,41 @@
 package monitoring
 
-import "net/http"
+import (
+	"log"
+	"net/http"
 
-type Prometheus struct{}
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Prometheus struct {
+	mx *http.ServeMux
+}
+
+func NewPrometheus(configs ...PrometheusConfig) *Prometheus {
+	p := &Prometheus{
+		mx: http.NewServeMux(),
+	}
+	for _, config := range configs {
+		config(p)
+	}
+	return p
+}
+
+type PrometheusConfig func(*Prometheus)
+
+func WithPromHTTP() PrometheusConfig {
+	return func(p *Prometheus) {
+		p.mx.Handle("/metrics", promhttp.Handler())
+	}
+}
 
 func (p *Prometheus) Monitor() {
 	srv := &http.Server{
-		Addr: ":9090",
+		Addr:    ":9090",
+		Handler: p.mx,
+	}
+	log.Println("prometheus metrics is listening on port 9090 ...")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println(err.Error())
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	_ "simple_twitter/internal/db/postgresql"
+	"simple_twitter/internal/monitoring"
 	"simple_twitter/internal/server"
 )
 
 func main() {
+
+	p := monitoring.NewPrometheus(monitoring.WithPromHTTP())
+	go p.Monitor()
+
 	srv := server.NewHTTPServer()
 	if err := srv.ListenAndServe(); err != nil {
 		panic(err)
