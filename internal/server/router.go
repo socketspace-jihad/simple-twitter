2f2e60c1d0c5e844e39b919c2de0446ab16f326f
Muhammad Abdurrohman <mabdurrohman26@dev1.idserve.net>
2026-02-04 16:50:47 +0700

    add req body and resp body to specific endpoint

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"simple_twitter/internal/logger"
 	"simple_twitter/internal/monitoring"
@@ -9,21 +11,61 @@ import (
 	"github.com/rs/zerolog/hlog"
 )
 
+type responseRecorder struct {
+	http.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (rr *responseRecorder) Write(b []byte) (int, error) {
+	rr.body.Write(b)                  // Capture the data
+	return rr.ResponseWriter.Write(b) // Send to client
+}
+
+func BodyLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var reqBody []byte
+		if r.Body != nil {
+			reqBody, _ = io.ReadAll(r.Body)
+			r.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+		}
+
+		rr := &responseRecorder{
+			ResponseWriter: w,
+			body:           &bytes.Buffer{},
+		}
+
+		next.ServeHTTP(rr, r)
+
+		hlog.FromRequest(r).Info().
+			Bytes("request_body", reqBody).
+			Str("response_body", rr.body.String()).
+			Msg("Body Details")
+	})
+}
+
 func StartRouter() http.Handler {
 	mx := http.NewServeMux()
 
 	mx.HandleFunc("/", ListPost)
-	mx.HandleFunc("/user/login", LoginHandler)
-	mx.HandleFunc("/user/logout", LogoutHandler)
-	mx.HandleFunc("/user/register", CreateUserHandler)
+	mx.HandleFunc("GET /user/login", LoginHandler)
+	mx.Handle("POST /user/login", BodyLogger(http.HandlerFunc(LoginHandler)))
 
-	mx.HandleFunc("/posts/create", CreatePostHandler)
+	mx.HandleFunc("GET /user/logout", LoginHandler)
+	mx.Handle("POST /user/logout", BodyLogger(http.HandlerFunc(LogoutHandler)))
+
+	mx.HandleFunc("GET /user/register", CreateUserHandler)
+	mx.Handle("POST /user/register", BodyLogger(http.HandlerFunc(CreateUserHandler)))
+
+	mx.Handle("POST /posts/create", BodyLogger(http.HandlerFunc(CreatePostHandler)))
+
 	mx.HandleFunc("/post", DetailPostHandler)
 	mx.HandleFunc("/posts/update", UpdatePostHandler)
 	var chain http.Handler = mx
 
 	chain = hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
 		hlog.FromRequest(r).Info().
+			Str("method", r.Method).
+			Str("url", r.URL.String()).
 			Int("status", status).
 			Int("size", size).
 			Dur("duration", duration).
